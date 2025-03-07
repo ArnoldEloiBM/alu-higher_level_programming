1ff18f3fe9ab6ddb158ccb6bb9b8282ec35d3a1c
ArnoldEloiBM <a.buyangem@alustudent.com>
2025-03-07 04:34:11 -0800

    my list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-# 2-is_same_class.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a class-checking function."""
+"""Define a List class"""
 
 
-def is_same_class(obj, a_class):
-    """Check if an object is exactly an instance of a given class.
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is exactly an instance of a_class - True.
-        Otherwise - False.
-    """
-    if type(obj) == a_class:
-        return True
-    return False
+class MyList(list):
+    """ A subclass of list"""
+    def __init__(self):
+        """Initializes of list"""
+        super().__init__()
+
+    def print_sorted(self):
+        """print a sorted of list(ascending order)"""
+        ascended_list = sorted(self)
+        print(ascended_list)
