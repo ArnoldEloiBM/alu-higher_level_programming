87039fcf9dadf089aad144f5ce1d7e83ee8f7b9f
ArnoldEloiBM <a.buyangem@alustudent.com>
2025-06-04 10:01:59 -0700

    new changes

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,12 +1,31 @@
 #!/usr/bin/node
+
+const request = require('request');
 const [, , URL] = process.argv;
-require('request')(URL, (_, __, body) => {
-  console.log(JSON.parse(body).reduce((acc, usr) => {
-    return usr.completed
-      ? {
-          ...acc,
-          [usr.userId]: (acc[usr.userId] || 0) + 1
-        }
-      : acc;
-  }, {}));
+
+request(URL, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  try {
+    const data = JSON.parse(body);
+
+    if (!Array.isArray(data)) {
+      throw new Error('Expected an array of tasks');
+    }
+
+    const completedTasks = data.reduce((acc, task) => {
+      if (task.completed) {
+        acc[task.userId] = (acc[task.userId] || 0) + 1;
+      }
+      return acc;
+    }, {});
+
+    console.log(completedTasks);
+  } catch (e) {
+    console.error('Error parsing JSON or reducing:', e.message);
+  }
 });
+
